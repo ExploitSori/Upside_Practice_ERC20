92ab7ff43edd6bb132322fa9aabd2a41f374ec2a
Sori <ljgggg1234@gmail.com>
2024-08-11 19:43:34 +0900

    erc20 solve

--- src/ERC20.sol
+++ src/ERC20.sol
@@ -129,6 +129,7 @@ contract ERC20 is IERC20, IERC20Permit{
         address spender = msg.sender;
         _spendAllowance(from, spender, value);
         _transfer(from, to, value);
+		emit Transfer(from, to, value);
         return true;
     }
     function _mint(address to, uint256 amount) internal{
@@ -138,6 +139,7 @@ contract ERC20 is IERC20, IERC20Permit{
     }
 	function mint(address to, uint256 amount) public{
 		mint(to, amount);
+
 	}
 
 	function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s) public{
@@ -167,7 +169,7 @@ contract ERC20 is IERC20, IERC20Permit{
 		bytes32 _DOMAIN_SEPARATOR = keccak256(abi.encode(keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),keccak256(bytes(name())), keccak256(bytes(abi.encodePacked(versions))), block.chainid, address(this)));
 		return _DOMAIN_SEPARATOR;
 	}
-	function _toTypedDataHash(bytes32 hash) public returns(bytes32){
+	function _toTypedDataHash(bytes32 hash) public view returns(bytes32){
 		bytes32 separator = DOMAIN_SEPARATOR();
 		bytes32 digest = keccak256(abi.encodePacked("\x19\x01", separator, hash));
 		return digest;
